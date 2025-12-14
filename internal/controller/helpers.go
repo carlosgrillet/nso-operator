b19a2e685f71cb4275a68f1e23569792b8e3404c
Cassandra Bv <cssbv98@gmail.com>
2025-12-14 16:55:34 +0100

    removed merge info

--- internal/controller/helpers.go
+++ internal/controller/helpers.go
@@ -47,11 +47,7 @@ func ensureObjectExists(ctx context.Context, c client.Client, obj client.Object)
 	log.Info("Resource exists, updating to match desired state")
 	obj.SetResourceVersion(existing.GetResourceVersion())
 	err = c.Update(ctx, obj)
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> cef3eca6572d766932f8e34c51cd38916944db57
 	if err != nil {
 		log.Error(err, "Failed to update resource")
 		return false, err
